d4e102792b1785d47b67ca46ce76902660f57c53
MirkoGLostia <giggi.torre@mail.com>
2023-11-20 14:59:42 +0100

    text exercise

--- src/main/java/org/exercise/java/springlamiapizzeriacrud/model/Ingredient.java
+++ src/main/java/org/exercise/java/springlamiapizzeriacrud/model/Ingredient.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class Ingredient {
 
     @Id
-    @GeneratedValue (strategy = GenerationType.AUTO)
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @NotBlank
